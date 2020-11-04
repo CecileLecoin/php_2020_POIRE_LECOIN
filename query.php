8c09c939814784f6cb08f1f92716bc9685c5e160
CecileLecoin <45843292+CecileLecoin@users.noreply.github.com>
2020-11-04 21:52:52 +0100

    f

--- query.php
+++ query.php
@@ -11,11 +11,11 @@ function listArticle() //donne tous les articles et les ecrit + bonus = clicker 
     {
         $arr[]= new Article($row->idnews, $row->idtheme ,$row->titrenews , $row->datenews, $row->textenews, $row->idredacteur);
         echo "<tr>";
-        echo "<td>" . $row->idnews . "</td>";
-        echo "<td>" . $row->idtheme . "</td>";
-        echo '<td id=titre><form method ="get" action ="lectureArticle.php"><input type="text" id="titrenews" name="titrenews" placeholder="'.$row->titrenews.'" value ="'.$row->titrenews.'" readonly><input type="submit" value="Voir plus"></form></td>';
-        echo "<td>" . $row->datenews . "</td>";
-        echo "<td>" . $row->textenews . "</td>";
+        /*echo "<td>" . $row->idnews . "</td></br>";
+        echo "<td>" . $row->idtheme . "</td></br>";*/
+        echo '<td id=titre><form method ="get" action ="lectureArticle.php"><input type="text" id="titrenews" name="titrenews" placeholder="'.$row->titrenews.'" value ="'.$row->titrenews.'" readonly><input type="submit" value="Voir plus"></form></td></br></br></br>';
+        echo "<td>" . $row->datenews . "</td></br></br>";
+        echo "<td>" . $row->textenews . "</td></br>";
         echo "</tr>";
     }
 }
@@ -74,11 +74,11 @@ function lireArticle(string $titre) // recupere un article par son titre
         $result2->bindParam(':id', $redac);
         $result2->execute();  
         while ($row=$result2->fetch(PDO::FETCH_OBJ))
-    {
-        echo "<td>" . $row->nom . "</td>";
-        echo "<td>" . $row->prenom . "</td>";      
-        echo "</tr>";
-    }
+        {
+            echo "<td>" . $row->nom . "</td>";
+            echo "<td>" . $row->prenom . "</td>";      
+            echo "</tr>";
+        }
     }
         
 
@@ -92,13 +92,15 @@ function getRedactorById(int $id) {
     $result = $objPdo->prepare('SELECT * FROM redacteur WHERE idredacteur=:id');
     $result->bindParam(':id', $id);
     $result->execute();
-    while ($)
-    if ($row == null) {
-        unset($objPdo);
-        return null;
-    } else {
-        unset($objPdo);
-        return '<p> id="loggedUserName"'.$row['nom'].'<br /> id="loggedUserMail"'.$row['mail'].'</p>';
+    while ($row = $result->fetch(PDO::FETCH_OBJ))
+    {
+        if ($row == null) {
+            unset($objPdo);
+            return null;
+        } else {
+            unset($objPdo);
+            return '<p> id="loggedUserName"'.$row['nom'].'<br /> id="loggedUserMail"'.$row['mail'].'</p>';
+        }
     }
 }
 
@@ -109,7 +111,12 @@ function mailRedacteurIsAlreadyUse(string $mail)
     $result = $objPdo->prepare('SELECT idredacteur FROM redacteur WHERE adressemail=:mail');
     $result->bindParam(':mail', $mail);
     $result->execute();
-
+    while ($row = $result->fetch(PDO::FETCH_OBJ)) 
+    {
+        if ($row == "") {
+            // return 
+        }
+    }
 }
 
 ?>
