3ea296516e0580484e3fcc88ede0e7bc587622bf
CecileLecoin <45843292+CecileLecoin@users.noreply.github.com>
2020-11-04 21:14:57 +0100

    f

--- query.php
+++ query.php
@@ -70,11 +70,11 @@ function lireArticle(string $titre) // recupere un article par son titre
         echo "<td>" . $row->datenews . "</td>";
         echo "<td>" . $row->textenews . "</td>";
         $redac= $row->idredacteur;
-        echo $redac;
     }
         $result2 = $objPdo->prepare('SELECT nom, prenom FROM redacteur WHERE idredacteur=:id');
         $result2->bindParam(':id', $redac);
         $result2->execute();  
+        $row=$result->fetch(PDO::FETCH_OBJ);
         echo "<td>" . $row->nom . "</td>";
         echo "<td>" . $row->prenom . "</td>";      
         echo "</tr>";
