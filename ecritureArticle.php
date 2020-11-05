22db316865f3592664c2563e389e0a48524d8144
Cecile Lecoin <lecoin2u@etu.univ-lorraine.fr>
2020-11-05 12:44:38 +0100

    f

--- ecritureArticle.php
+++ ecritureArticle.php
@@ -10,10 +10,12 @@
 
         <?php
         include_once 'query.php';
+        include_once 'index.php';
             function listeTheme() {
                 include_once 'query.php';
                 include_once 'index.php';
                 $i=0;
+                echo "there";
                 getDescriptionTheme();
                 foreach($arrTheme as $value)
                 {
@@ -27,12 +29,12 @@
 				<input type="text" id="idnews" name="idnews" size="3" maxlength="5" placeholder="à changer" value="">-->
                 
                 <label for="theme">Id theme :</label>
-                <select id="theme" name="theme" required><?php listeTheme() ?></select>
+                <select name="theme" required> <?php listeTheme() ?> </select>
                 
                 <br /><br />
                 
                 <label for="textenews">News :</label>
-				<input type="text" id="textenews" name="textenews" size="50" maxlength="299" value="" required>
+				<input type="text" id="textenews" name="textenews" width="100" heigth="200" maxlength="299" value="" required>
 				<br /><br />
                 <input type="submit" name="valider" value="Valider">
                 
