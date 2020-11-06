e4ff07d21aaf0556225169bbdd4ae5463acc8c13
Cecile Lecoin <lecoin2u@etu.univ-lorraine.fr>
2020-11-06 15:42:08 +0100

    f

--- query.php
+++ query.php
@@ -14,7 +14,7 @@ function listArticle() //donne tous les articles et les ecrit + bonus = clicker 
         /*echo "<td>" . $row->idnews . "</td></br>";
         echo "<td>" . $row->idtheme . "</td></br>";*/ //inutile à l'affichage
         echo "</tr><tr><td>" . $row->datenews . "</td>";
-        echo '<td id=titre><form method ="get" action ="lectureArticle.php"><input type="text" id="titrenews" name="titrenews" placeholder="'.$row->titrenews.'" value ="'.$row->titrenews.'" readonly><input type="submit" value="Voir plus"></form></td></br></br></br>';
+        echo '<td id=titre><form method ="get" action ="lectureArticle.php" ><input type="text" id="titrenews" name="titrenews" class="titre-pass" placeholder="'.$row->titrenews.'" value ="'.$row->titrenews.'" readonly><input type="submit" value="Voir plus"></form></td></br></br></br>';
         
         echo "<td>" . $row->textenews . "</td>";
         echo "</tr>";
