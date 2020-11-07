7fc7c31c5fbb515bfc301a5cd16b3b6d12c7dcbd
CecileLecoin <45843292+CecileLecoin@users.noreply.github.com>
2020-11-07 13:41:22 +0100

    f

--- index.php
+++ index.php
@@ -20,8 +20,8 @@ session_start();
 			if ($auth) {
 				echo "id = " . $_SESSION['id'];
 				$co .= getRedactorById((int)$_SESSION['id']);
-				echo "<a href=../ecritureArticle.php>Ecrire un article</a></br></br>";
-				echo "<a href=../disconnect.php>Déconnexion</a></br></br>";
+				echo "<a href=../view/ecritureArticle.php>Ecrire un article</a></br></br>";
+				echo "<a href=../control/disconnect.php>Déconnexion</a></br></br>";
 			} else {
 				$co .= 'Déconnecté</br>';
 			}
